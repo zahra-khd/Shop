21ed507714ef6c5fca317c5772e4a32b24b23852
zahra-khd <167901305+zahra-khd@users.noreply.github.com>
2024-05-11 23:37:44 +0330

    refactor: improve activities

--- src/components/profile/Activities/Activities.jsx
+++ src/components/profile/Activities/Activities.jsx
@@ -1,21 +1,56 @@
-import { Comments } from "./Comments";
-import { ExitProfile } from "./ExitProfile";
-import { GiftCard } from "./GiftCard";
-import { Messages } from "./Messages";
-import { Orders } from "./Orders";
-import { ProfileMe } from "./ProfileMe";
-import { RecentVisits } from "./RecentVisits";
+
 
 export const Activities = () => {
+  const tabs = [
+    {
+      title: "سفارش ها",
+      icon: <i className="bi bi-bag-heart icon-size"></i>,
+      route: "orders",
+    },
+    {
+      title: "دیدگاه ها",
+      icon: <i className="bi bi-chat icon-size"></i>,
+      route: "comments",
+    },
+    {
+      title: "کارت هدیه",
+      icon: <i className="bi bi-gift icon-size"></i>,
+      route: "GiftCard",
+    },
+    {
+      title: "پیام ها",
+      icon: <i className="bi bi-bell icon-size"></i>,
+      route: "Messages",
+    },
+    {
+      title: "بازدید های اخیر",
+      icon: <i className="bi bi-clock icon-size"></i>,
+      route: "RecentVisits",
+    },
+    {
+      title: "اطلاعات نام کاربری",
+      icon: <i className="bi bi-person icon-size"></i>,
+      route: "ProfileMe",
+    },
+    {
+      title: "خروج",
+      icon: <i className="bi bi-box-arrow-right icon-size"></i>,
+      route: "ExitProfile",
+    }
+  ];
+
   return (
     <div className="text-section-start">
-      <Orders />
-      <Comments />
-      <GiftCard />
-      <Messages />
-      <RecentVisits />
-      <ProfileMe />
-      <ExitProfile/>
+      {tabs.map((tab, index) => (
+        <div className="d-flex h-100 text-hover" key={index}>
+          <div className="border-top-orders mx-2 p-2 w-100">
+           {tab.icon}
+            <span className="fw-bold pe-3 pt-1">{tab.title} </span>
+          </div>
+        </div>
+      ))}
+
+    
     </div>
   );
 };
