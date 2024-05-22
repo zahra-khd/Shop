940a5e1999821c13619c896ec43208b8773f0122
zahra-khd <167901305+zahra-khd@users.noreply.github.com>
2024-05-22 19:30:32 +0330

    feat(Comment): created page comment

--- src/components/profile/Activities/Comment.jsx
+++ src/components/profile/Activities/Comment.jsx
@@ -1,12 +1,13 @@
-
-
-
-
 export const Comment = () => {
-
   return (
-    <div >
-        Comment
-    </div>
+    <section className="style-section text-section-start">
+      <div className="mx-3 my-4">
+        <h6>دیدگاه ها</h6>
+      </div>
+      <div className="border-bottom pb-2">
+        <span className="mx-3 text-muted">در انتظار دیدگاه</span>
+        <span className="text-muted mx-3"> دیدگاه های من</span>
+      </div>
+    </section>
   );
 };
