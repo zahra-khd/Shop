a16a63714056441817b68cc50e7ffb80e3b72aca
zahra-khd <167901305+zahra-khd@users.noreply.github.com>
2024-05-22 19:31:39 +0330

    feat(Gift card): created page gift card

--- src/components/profile/Activities/GiftCard.jsx
+++ src/components/profile/Activities/GiftCard.jsx
@@ -3,8 +3,14 @@
 export const GiftCard = () => {
 
     return (
-      <div >
-          GiftCard
+      <section className="style-section text-section-start">
+      <div className="mx-3 my-4">
+        <h6>کارت های هدیه</h6>
       </div>
+      <div className="border-bottom pb-2">
+        <span className="mx-3 text-muted">هدیه گرفتم </span>
+        <span className="text-muted mx-3"> هدیه دادم</span>
+      </div>
+    </section>
     );
   };
