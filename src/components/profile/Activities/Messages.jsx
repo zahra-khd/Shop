e6cc71d7f8feadfbf28d335a413923499789efe3
zahra-khd <167901305+zahra-khd@users.noreply.github.com>
2024-05-22 20:46:58 +0330

    feat: created page message

--- src/components/profile/Activities/Messages.jsx
+++ src/components/profile/Activities/Messages.jsx
@@ -1,10 +1,18 @@
-
-
 export const Messages = () => {
-
-    return (
-      <div >
-          Messages
+  return (
+    <section className="style-section text-section-start">
+      <div className="mx-4 my-3 pb-2 style-text-message">
+        <h6>پیغام ها</h6>
       </div>
-    );
-  };
+      <div className="border-bottom pb-2 d-flex">
+        <button className=" button-style mx-3 text-muted">همه پیام ها</button>
+        <button className="button-style text-muted d-flex">
+           <div className="rotate-icon ms-1">
+              <i className="bi bi-megaphone-fill"></i>
+            </div>
+            {"پیشنهاد ها "}
+        </button>
+      </div>
+    </section>
+  );
+};
