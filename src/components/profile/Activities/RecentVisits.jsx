dda351b3594aa0495f9105b50b3002eeb56f469b
zahra-khd <167901305+zahra-khd@users.noreply.github.com>
2024-05-22 21:04:41 +0330

    feat(Recent visits): created page Recent Visits

--- src/components/profile/Activities/RecentVisits.jsx
+++ src/components/profile/Activities/RecentVisits.jsx
@@ -1,8 +1,9 @@
 
 
-
-export const RecentVisits=()=>{
-    return(
-        <div>RecentVisits</div>
-    )
-}
+export const RecentVisits = () => {
+  return (
+    <section className="style-section text-section-start">
+       <h6 className="mx-4 my-3 pb-2  style-text-message"> بازدید های اخیر </h6>
+    </section>
+  );
+};
